cacf5d036d3f42003dbdc5a1eacaa464e40e1e31
scainburger <scainburger@gmail.com>
2015-09-03 01:44:53 +1000

    Initial Commit - it's done though :)

---
+++ RollABall/Assets/Scripts/PickupController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		// Time.deltaTime Makes it framerate independant, therefore smoother
+	}
+}
---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		// Get a reference to the Player's RigidBody controller
+		rb = GetComponent<Rigidbody> ();
+		countText.text = "Score: " + count.ToString();
+		winText.text = "";
+	}
+
+	// Called before performing physics calculations
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	// Called whenever this object touches another
+	void OnTriggerEnter (Collider other)
+	{
+		// For this to work, must check "Is Trigger" in the Pickup's Box Collider
+
+		// Set a tag in the inspector, under the name.
+		if (other.gameObject.CompareTag ("Pickup")) 
+		{
+			// Deactivate the object
+			other.gameObject.SetActive (false);
+			count++;
+			countText.text = "Score: " + count.ToString();
+			if (count == 16)
+			{
+				winText.text = "You Win!";
+			}
+
+			// Add a RigidBody to this, and tick "Is Kinematic", meaning it isn't affected by physics
+
+			// Anything with a collider and a RigidBody is considered Dynamic, 
+			// and is more efficient to use for animated objects, such as this.
+		}
+	}
+}
